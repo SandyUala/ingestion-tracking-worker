7f4143a58b5665c750d11eb775dab4c474f65299
Alois Barreras <alois@vicino.io>
2017-06-05 16:36:33 -0400

    updating to node 8, updating packages

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,20 @@
-FROM astronomerio/kinesis-consumer:v4.1.0
+FROM node:8.0.0-alpine
+
+MAINTAINER Alois Barreras <alois@astronomer.io>
+
+# Install java for the kcl
+RUN apk --update add openjdk7-jre
+
+RUN mkdir -p /usr/src/app
+RUN mkdir -p /usr/src/app/log
+
+# Bundle app source
+WORKDIR /usr/src/app
+COPY package.json /usr/src/app
+COPY package-lock.json /usr/src/app
+RUN npm install --production
+
+COPY lib /usr/src/app/lib
+COPY properties /usr/src/app/properties
+
+CMD ["npm", "run", "dev"]
--- test/index.test.js
+++ test/index.test.js
@@ -1,18 +1,21 @@
-const { stub, spy } = require('sinon');
-const fs = require('fs');
 const { assert } = require('chai');
+const createLogger = require('@astronomerio/astronomer-logger');
 const IngestionTracking = require('../lib/record-processor.js');
 
-describe('Kinesis Consumer Ingestion Tracking', function () {
+describe('Kinesis Consumer Ingestion Tracking', () => {
   let consumer;
 
-  beforeEach(function () {
+  beforeEach(() => {
     process.env.ASTRONOMER_APP_ID = 'app_id';
     process.env.ASTRONOMER_STREAM_NAME = 'stream';
-    consumer = new IngestionTracking();
+    consumer = new IngestionTracking({
+      logger: createLogger('integrator-worker', {
+        logPath: './',
+      }),
+    });
   });
 
-  it('should initialize with correct properties', function () {
+  it('should initialize with correct properties', () => {
     assert.ok(consumer.analytics);
   });
 });
