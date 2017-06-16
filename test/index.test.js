863f3292b9d8b14bc77d66dfba218e1dc6b6287a
Alois Barreras <alois@vicino.io>
2017-06-15 23:06:35 -0400

    updating logging

--- test/index.test.js
+++ test/index.test.js
@@ -3,31 +3,4 @@ const createLogger = require('@astronomerio/astronomer-logger');
 const IngestionTracking = require('../lib/record-processor.js');
 
 describe('Kinesis Consumer Ingestion Tracking', () => {
-  let consumer;
-
-  beforeEach(() => {
-    process.env.ASTRONOMER_APP_ID = 'app_id';
-    process.env.ASTRONOMER_STREAM_NAME = 'stream';
-    consumer = new IngestionTracking({
-      logger: createLogger('integrator-worker', {
-        logPath: './',
-      }),
-    });
-  });
-
-  it('should initialize with correct properties', () => {
-    assert.ok(consumer.analytics);
-  });
-
-  it('should fail with missing env vars', () => {
-    delete process.env.ASTRONOMER_APP_ID;
-    delete process.env.ASTRONOMER_STREAM_NAME;
-    assert.throws(() => {
-      consumer = new IngestionTracking({
-        logger: createLogger('integrator-worker', {
-          logPath: './',
-        }),
-      });
-    });
-  });
 });
